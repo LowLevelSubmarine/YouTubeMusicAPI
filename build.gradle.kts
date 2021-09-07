0d6c43fa04fc05560c2ecf4fa42242a31a4d4a1e
LowLevelSubmarine <>
2021-09-07 17:59:17 +0200

    added kotlin stdlib

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.10")
     implementation("com.google.code.gson:gson:2.8.8")
     testImplementation(kotlin("script-runtime"))
 }
